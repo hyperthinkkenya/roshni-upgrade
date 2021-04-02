77594767ac6fa47cc45d125bc1b3c559daeb9b77
John Gaitho <johngaitho05@gmail.com>
2021-04-02 12:14:29 +0300

    evaluate on change

--- restrict_discount_above_100/models/sale_order_line_inherit.py
+++ restrict_discount_above_100/models/sale_order_line_inherit.py
@@ -13,9 +13,13 @@ from odoo.exceptions import UserError
 class SaleOrderLine(models.Model):
     _inherit = 'sale.order.line'
 
-    @api.model
     @api.constrains('discount')
     def restrict_discount_above_100(self):
+        if any(line.discount > 100 for line in self):
+            raise UserError(_("Discount value cannot be greater than 100"))
+
+    @api.onchange('discount')
+    def onchnage_discount(self):
         if self.discount > 100:
             raise UserError(_("Discount value cannot be greater than 100"))
 
@@ -23,8 +27,12 @@ class SaleOrderLine(models.Model):
 class AccountMoveLine(models.Model):
     _inherit = 'account.move.line'
 
-    @api.model
     @api.constrains('discount')
     def restrict_discount_above_100(self):
+        if any(line.discount > 100 for line in self):
+            raise UserError(_("Discount value cannot be greater than 100"))
+
+    @api.onchange('discount')
+    def onchnage_discount(self):
         if self.discount > 100:
             raise UserError(_("Discount value cannot be greater than 100"))
